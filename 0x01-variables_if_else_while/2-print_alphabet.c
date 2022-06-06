16595ccff8ad0a8afa95091a777bc034d5b81082
Confiee <avhatakale@gmail.com>
2022-06-06 15:53:18 -0700

    task 2 fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,41 +1,20 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - Program entry point
- *
- *   * Description: Print all possible different combinations of two digits
- *
+ *   * Description: Prints the letters of the alphabet in lowercase
  *    * Return: 0
- *
  **/
-
 int main(void)
-
 {
+char x = 'a';
 
-int x;
-int y;
-for(x = 0; x < 9; x++)                                                
-{
-for (y = x + 1; y < 10; y++)
-{
-																 putchar((x % 10) + '0');
- putchar((y % 10) + '0');												 
-if (x == 8 && y == 9)
-
-																continue;
-																putchar(',');
-
-																putchar(' ');
-
-																}
-
+while (x <= 'z')
+					{
+putchar(x);
+x++;
 }
 
 putchar('\n');
 return (0);
-
 }
-
-
