9c2c2321f87bcdfbd0bbeb350f269183b6b83b91
Confiee <avhatakale@gmail.com>
2022-06-06 15:25:11 -0700

    task 7 fixed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 
-	char x = 'z';
+char x = 'z';
 
 while (x >= 'a')
 
-	{
-	       putchar(x);
-	       x--;
-	}
+{
+putchar(x);
+x--;
+}
 
-	putchar('\n')
-	return (0);
+putchar('\n')
+return (0);
 }
