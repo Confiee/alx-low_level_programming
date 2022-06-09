f69a1f95f6003c9ad67df91ded4ae03e6a001171
Confiee <avhatakale@gmail.com>
2022-06-09 09:50:13 -0700

    Task 8

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,10 @@
 #include "main.h"
 
-
-
 /**
  *
  * * print_alphabet_x10 -> prints the lowercase alphabets
  *
  **/
-
-
 
 void print_alphabet_x10(void)
 
@@ -18,15 +14,15 @@ int j;
 
 int x;
 
-
-
 for (x = 0; x < 10; x++)
 
 {
 
 for (j = 'a'; j <= 'z'; j++)
+
 {												
 _putchar(j);															
+
 }
 _putchar('\n');
 
